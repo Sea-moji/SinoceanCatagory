c5a9ca49dd6d2cf72e4226878a6ca58f40648678
Sea-moji <zwcsssapple@163.com>
2021-06-07 15:12:31 +0800

    add base project

---
+++ SinoceanCatagory/SinoceanCatagory.h
@@ -0,0 +1,18 @@
+//
+//  SinoceanCatagory.h
+//  SinoceanCatagory
+//
+//  Created by sinocean on 2021/6/7.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SinoceanCatagory.
+FOUNDATION_EXPORT double SinoceanCatagoryVersionNumber;
+
+//! Project version string for SinoceanCatagory.
+FOUNDATION_EXPORT const unsigned char SinoceanCatagoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SinoceanCatagory/PublicHeader.h>
+
+
